91edf08c3efd0352a34b83f51bc0852aa2205d83
yangs <yangs@gisuni.com>
2018-08-22 17:43:53 +0800

    Convert scss to css using Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 let gulp = require('gulp');
+// Requires the gulp-sass plugin
+let sass = require('gulp-sass');
 
 gulp.task('hello', () => {
   console.log('Hello World');
 });
+
+gulp.task('sass', () =>
+  gulp.src('app/scss/styles.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+);
