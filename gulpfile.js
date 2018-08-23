491fd4053add25a1a81c52d0a952acae05bdca02
WillieYang <849346604@qq.com>
2018-08-23 23:56:41 +0800

    Combination of browser-sync and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 let gulp = require('gulp');
 // Requires the gulp-sass plugin
 let sass = require('gulp-sass');
+// Requires the browser-sync plugin
+let browserSync = require('browser-sync').create();
 
 gulp.task('hello', () => {
   console.log('Hello World');
@@ -8,10 +10,22 @@ gulp.task('hello', () => {
 
 gulp.task('sass', () =>
   gulp.src('app/scss/**/*.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('app/css'))
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 );
 
-gulp.task('watch', () =>
+gulp.task('browserSync', () =>
+  browserSync.init({
+    injectChanges: true,
+    server: {
+      baseDir: 'app'
+    },
+  })
+);
+
+gulp.task('watch', ['sass', 'browserSync'], () =>
   gulp.watch('app/scss/**/*.scss', ['sass'])
 );
